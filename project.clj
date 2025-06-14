7fcfacdb99381aa0e97b17692914e4b822742e95
ruroru <111705692+ruroru@users.noreply.github.com>
2025-06-14 19:03:00 +0000

    add bump-md plugin

--- project.clj
+++ project.clj
@@ -13,6 +13,7 @@
                  [commons-io/commons-io "2.19.0"]
                  [org.clojars.jj/surykatka "1.0.0"]]
   :plugins [[org.clojars.jj/bump "1.0.4"]
+            [org.clojars.jj/bump-md "1.0.0"]
             [org.clojars.jj/strict-check "1.0.2"]]
 
   :repl-options {:init-ns unarchive.core})
